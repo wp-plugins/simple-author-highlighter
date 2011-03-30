9f977f287b35f2f16c67301612cbf07a02ee629f
jimaek <jimaek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-30 11:09:44 +0000

    tagging version 0.6.5
    git-svn-id: https://plugins.svn.wordpress.org/simple-author-highlighter/trunk@366868 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sah.php
+++ sah.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple Author Highlighter
 Plugin URI: http://www.dakulov.eu#page5
 Description: Easy highlight authors comments
-Version: 0.6
+Version: 0.6.5
 Author: Akulov Dmitriy
 Author URI: http://www.dakulov.eu
 */
@@ -63,13 +63,17 @@ function sah_register_mysettings() {
 function sah_settings_page() {
 ?>
 <div class="wrap">
+<div id="icon-options-general" class="icon32"></div>
 <h2>Simple Author Highlighter - Settings</h2>
 <br/>
 <form method="post" action="options.php">
     <?php settings_fields( 'sah-settings-group' ); ?>
+	
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Enter the highlight color</th>
+		<h2>Main - Author Highlighter</h2>
+		<h4>Select the colors to highlight the comments of the post authors</h4>
+		        <th scope="row">Enter the highlight color</th>
         <td><input type="text" name="color_code" value="<?php echo get_option('color_code'); ?>" /> <i>For example: #b6bdf6 Leave blank for theme's default</i><br/></td>
         </tr>
 		<tr valign="top">
@@ -77,9 +81,10 @@ function sah_settings_page() {
 		<td><input type="text" name="color_code2" value="<?php echo get_option('color_code2'); ?>" /> <i>For example: #ffffff Leave blank for theme's default</i></td>
 		</tr>
 	</table>
-	<h2>Global highlight users</h2>
+	<h2>(Optional) - Global User Highlighter </h2>
 	 <table class="form-table">
     <tr valign="top">
+	<h4>Select the colors to highlight the comments of the selected users</h4>
         <th scope="row">Enter the users login names</th>
         <td><input type="text" name="user" value="<?php echo get_option('user'); ?>" /> <i>For example: admin For multiple users use comma as a delimiter</i><br/></td>
         </tr>
@@ -98,6 +103,6 @@ function sah_settings_page() {
 
 </form>
 <br/><center>
-If you have questions visit our  <a target="_blank" class="button-primary" href="http://www.dakulov.eu#page5">website</a> | If you liked the plugin please <a target="_blank" class="button-primary" href="http://wordpress.org/extend/plugins/simple-author-highlighter/">vote</a> for us </center>
+If you have any questions or problems leave a comment <a target="_blank" class="button-secondary" href="http://www.dakulov.eu#page5">website</a> | If you liked the plugin please <a target="_blank" class="button-secondary" href="http://wordpress.org/extend/plugins/simple-author-highlighter/">vote</a> for us </center>
 </div>
 <?php } ?>
